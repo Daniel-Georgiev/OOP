f99046a4892ec7b70b7ef6b440334e8347776b8d
Daniel-Georgiev <daniko91@mail.bg>
2015-01-23 16:37:12 +0200

    StaticMembers

---
+++ Static-Members/Static-Members/PointIn3d.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point3d
+{
+    class PointIn3d
+    {
+        static void Main(string[] args)
+        {
+           Point3D pointA = new Point3D(5, 10, 10);
+            Console.WriteLine(pointA);
+        }
+    }
+
+}
+
